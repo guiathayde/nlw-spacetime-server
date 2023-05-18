d519668e374cab26cb9185c7df21918c0fb2d13b
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-18 08:27:19 -0300

    Authentication with GitHub OAuth and JWT

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 
+import { authRoutes } from './routes/auth';
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
@@ -9,6 +13,11 @@ app.register(cors, {
   origin: ['http://localhost:3000', 'https://spacetime.guiathayde.dev'],
 });
 
+app.register(jwt, {
+  secret: process.env.JWT_SECRET ?? 'spacetime',
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app
