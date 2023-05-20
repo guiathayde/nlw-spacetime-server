2b1ac2f8bf509adc3b55889bc96f3127281b8ff4
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-20 17:18:35 -0300

    Add createdAt field when get memories and remove limit size from files uploaded

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -23,6 +23,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
         id: memory.id,
         coverUrl: memory.coverUrl,
         excerpt: memory.content.substring(0, 115).concat('...'),
+        createdAt: memory.createdAt,
       };
     });
   });
@@ -47,7 +48,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory;
   });
 
-  app.post('/memories/:id', async (request) => {
+  app.post('/memories', async (request) => {
     const bodySchema = z.object({
       content: z.string(),
       coverUrl: z.string(),
