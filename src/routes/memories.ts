2d1a20ba69381b32d4a38f643ad951091de1607d
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-21 22:11:20 -0300

    Fix memory update

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -1,7 +1,22 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
+import { unlink } from 'node:fs';
+import { resolve } from 'node:path';
 
 import { prisma } from '../lib/prisma';
+
+const coverTypes = [
+  'gif',
+  'jpg',
+  'jpeg',
+  'png',
+  'mpg',
+  'mp2',
+  'mpeg',
+  'mpe',
+  'mpv',
+  'mp4',
+] as const;
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.addHook('preHandler', async (request) => {
@@ -19,10 +34,16 @@ export async function memoriesRoutes(app: FastifyInstance) {
     });
 
     return memories.map((memory) => {
+      let excerpt = memory.content;
+      if (excerpt.length > 115) {
+        excerpt = excerpt.substring(0, 115).concat('...');
+      }
+
       return {
         id: memory.id,
         coverUrl: memory.coverUrl,
-        excerpt: memory.content.substring(0, 115).concat('...'),
+        coverType: memory.coverType,
+        excerpt,
         createdAt: memory.createdAt,
       };
     });
@@ -35,9 +56,14 @@ export async function memoriesRoutes(app: FastifyInstance) {
 
     const { id } = paramsSchema.parse(request.params);
 
+    console.log(id);
+
     const memory = await prisma.memory.findUniqueOrThrow({
       where: {
         id,
+      },
+      include: {
+        user: true,
       },
     });
 
@@ -49,18 +75,24 @@ export async function memoriesRoutes(app: FastifyInstance) {
   });
 
   app.post('/memories', async (request) => {
+    console.log(JSON.stringify(request.body, null, 2));
+
     const bodySchema = z.object({
       content: z.string(),
       coverUrl: z.string(),
+      coverType: z.enum(coverTypes),
       isPublic: z.coerce.boolean().default(false),
     });
 
-    const { content, coverUrl, isPublic } = bodySchema.parse(request.body);
+    const { content, coverUrl, coverType, isPublic } = bodySchema.parse(
+      request.body
+    );
 
     const memory = await prisma.memory.create({
       data: {
         content,
         coverUrl,
+        coverType,
         isPublic,
         userId: request.user.sub,
       },
@@ -79,10 +111,13 @@ export async function memoriesRoutes(app: FastifyInstance) {
     const bodySchema = z.object({
       content: z.string(),
       coverUrl: z.string(),
+      coverType: z.enum(coverTypes),
       isPublic: z.coerce.boolean().default(false),
     });
 
-    const { content, coverUrl, isPublic } = bodySchema.parse(request.body);
+    const { content, coverUrl, coverType, isPublic } = bodySchema.parse(
+      request.body
+    );
 
     let memory = await prisma.memory.findUniqueOrThrow({
       where: {
@@ -94,6 +129,15 @@ export async function memoriesRoutes(app: FastifyInstance) {
       return reply.status(401).send();
     }
 
+    const oldMemory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+      select: {
+        coverUrl: true,
+      },
+    });
+
     memory = await prisma.memory.update({
       where: {
         id,
@@ -101,9 +145,20 @@ export async function memoriesRoutes(app: FastifyInstance) {
       data: {
         content,
         coverUrl,
+        coverType,
         isPublic,
       },
     });
+
+    const regex = /\/([^/]+)$/;
+    const match = oldMemory.coverUrl.match(regex);
+    if (match) {
+      const fileName = match[1];
+      const pathToFile = resolve(__dirname, '..', '..', 'uploads', fileName);
+      unlink(pathToFile, (err) => {
+        if (err) console.error(err);
+      });
+    }
 
     return memory;
   });
